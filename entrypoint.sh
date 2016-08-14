e21a0037b940336b1848a896111e57f811a7e67d
Alex Simenduev <shamil.si@gmail.com>
2016-08-14 13:04:17 +0300

    fix SMTP_HOST/PORT env variables retrieval

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ set -e # exit on error
 }
 
 export SMTP_LOGIN SMTP_PASSWORD
-export SMTP_HOST=${EXT_RELAY_HOST:-"email-smtp.us-east-1.amazonaws.com"}
-export SMTP_PORT=${EXT_RELAY_PORT:-"25"}
+export SMTP_HOST=${SMTP_HOST:-"email-smtp.us-east-1.amazonaws.com"}
+export SMTP_PORT=${SMTP_PORT:-"25"}
 export ACCEPTED_NETWORKS=${ACCEPTED_NETWORKS:-"192.168.0.0/16 172.16.0.0/12 10.0.0.0/8"}
 export USE_TLS=${USE_TLS:-"no"}
 export TLS_VERIFY=${TLS_VERIFY:-"may"}
